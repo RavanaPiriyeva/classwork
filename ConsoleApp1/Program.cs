166083246f2d65b7be48f981670975fd3ed66954
RavanaPiriyeva <7j59c43@code.edu.az>
2022-03-15 15:56:57 +0400

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region task1
+
+
+            //int[] numbers = { 5, 10, 15, 3, 7, 20, 50 };
+
+            //var sum = 0;
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+
+            //    sum += numbers[i];
+
+            //}
+            //Console.WriteLine("ededlerin cemi " + sum);
+
+            #endregion
+
+            #region task2
+            //string[] name = { "Ayten", "Asiman", "Revaee"};
+
+            // for (int i = 0; i < name.Length; i++)
+            // {
+            //    if (name[i].Length > 5)
+            //    {
+            //        Console.WriteLine(name[i]);
+            //    }
+            //  }
+
+            #endregion
+
+            #region task3
+            //int number1 = 2;
+            //int number2 = 5;
+            //int sum=0;
+            //for(int i = number1; i<number2; i++)
+            //{
+            //    sum += i;
+            //}
+            //Console.WriteLine(sum);
+            #endregion
+
+            #region task4
+            //int sum = 0;
+            //int count = 0;
+            //int result;
+
+            //for (int i = 1; i < 100; i++)
+            //{
+            //    if (i % 15 == 0)
+            //    {
+            //        sum += i;
+            //        count++;
+            //    }
+            //}
+            //result = sum / count;
+            //Console.WriteLine(result);
+            #endregion
+
+            #region task5
+            //int number = 345;
+            //var sum = 0;
+            //while (number > 0)
+            //{
+            //    sum += number % 10;
+            //    number /= 10;
+            //}
+            //Console.WriteLine(sum);
+            #endregion
+        }
+    }
+}
